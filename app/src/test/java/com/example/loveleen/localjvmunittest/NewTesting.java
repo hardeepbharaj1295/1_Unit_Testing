1b83ad68ca01760d8e9fe1140e695753f8faaf33
Hardeep Singh <hardeepbharaj1295@gmail.com>
2019-03-08 06:20:52 +0530

    Add New Testing File

---
+++ app/src/test/java/com/example/loveleen/localjvmunittest/NewTesting.java
@@ -0,0 +1,14 @@
+package com.example.loveleen.localjvmunittest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NewTesting {
+
+    @Test
+    public void addition() throws Exception
+    {
+        assertEquals(4,MyCalculatorModel.add(2,2));
+    }
+}
